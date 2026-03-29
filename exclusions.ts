b755e4add163ac73de59554929bf722b225aaac8
Tahmid Ahmed <tahmid@tahmid.io>
2026-03-29 01:44:17 -0400

    Setup test coverage uploading

---
+++ exclusions.ts
@@ -0,0 +1,11 @@
+/**
+ * See https://github.com/tahminator/sapling/blob/main/.github/scripts/src/test/index.ts
+ * for test exclusion usages.
+ */
+
+const baseDir = "src";
+
+export const exclusions = [
+  `${baseDir}/**/*.test.ts`,
+  `${baseDir}/**/*.test.tsx`,
+];
