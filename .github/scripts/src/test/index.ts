a94d696fd29a7ae424c68cb0eee4331b66e847ff
Tahmid Ahmed <tahmid@tahmid.io>
2026-03-29 01:53:41 -0400

    Setup test coverage uploading (#3)

---
+++ .github/scripts/src/test/index.ts
@@ -0,0 +1,50 @@
+import { SonarScannerClient, Utils } from "@tahminator/pipeline";
+import { $ } from "bun";
+
+import { exclusions } from "../../../../exclusions";
+
+async function main() {
+  const { sonarToken } = parseCiEnv(await Utils.getEnvVariables(["ci"]));
+
+  const sonarClient = new SonarScannerClient({
+    auth: {
+      token: sonarToken,
+    },
+    scan: {
+      additionalArgs: {
+        "javascript.lcov.reportPaths": "coverage/lcov.info",
+        "coverage.exclusions": `${exclusions}`,
+      },
+      organization: "tahminator",
+      sourceCodeDir: "src/",
+      projectKey: "tahminator_sapling",
+    },
+    run: {
+      runTestsCmd: $`pnpm run test`,
+    },
+  });
+
+  await sonarClient.runTests();
+  await sonarClient.uploadTests();
+}
+
+function parseCiEnv(ciEnv: Record<string, string>) {
+  const sonarToken = (() => {
+    const v = ciEnv["SONAR_TOKEN"];
+    if (!v) {
+      throw new Error("Missing SONAR_TOKEN from .env.ci");
+    }
+    return v;
+  })();
+
+  return { sonarToken };
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
